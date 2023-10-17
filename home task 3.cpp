e3b3307f6cb41be7dd3d01503ca5846c65dcc0ab
aatikakamran <147543080+aatikakamran@users.noreply.github.com>
2023-10-17 18:58:39 +0500

    Add files via upload

---
+++ home task 3.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+using namespace std;
+int main ()
+//{
+//	int x;
+//	cout<<"enter 1 for sindh , 2 for kpk , 3 for punjab , 4 for balochistan"<<endl;
+//	cin>>x;
+//	switch (x)  {
+//	 
+//	 case 1 : 
+//	 cout<<"the population of sindh is 47,854,510 "<<endl;
+//	 break;
+//	 case 2 : 
+//	 cout<<"the population of kpk is 30,508,920 "<<endl;
+//	 break;
+//	 case 3 : 
+//	 cout<<"the population of punjab is 109,989,655 "<<endl;
+//	 break;
+//	 case 4 : 
+//	 cout<<"the population of balochistan is 12,335,129 "<<endl;
+//	 break;
+//	 default :
+//	 	cout<<"the number entered is invalid, try again"<<endl;
+//	 	break;
+//	 
+//	 
+//	}
+//	return 0;
+//		
+//}
+
+//
+//{
+//	char x;
+//	cout<<"enter a character"<<endl;
+//	cin>>x;
+//	switch (x) {
+//	 
+//	 case  'a' :  case  'e' :  case 'i' : case  'o':  case  'u' : case 'A':  case 'E':  case 'I' : case  'O' : case  'U' :	
+//	 cout<<"the given character is a vowel"<<endl;
+//	break;
+//	case 'b':  case  'c' :  case  'd' : case  'f' : case  'g' : case  'h' : case  'j' : case 'k' : case  'l' : case 'm' :	
+//	case  'n':  case  'p' :  case 'q' : case  'r' : case 's' :  case  't' : case  'v' :  case  'w' :  case  'x' : case  'y' :
+//	case  'z' : case  'B':  case  'C' : case  'D' : case 'F' : case  'G' : case  'H'  : case  'J' : case  'K' : case  'L' :
+//	case  'M' : case  'N' : case  'P' : case  'Q' : case 'R' :  case  'S' : case 'T' : case  'V' :  case  'W' : case  'X' :
+//	case  'Y' : case  'Z' : 	
+//	 cout<<"the given character is a consonant"<<endl;
+//	break;
+//	default : 
+//	cout<<"the character is not an alphabet"<<endl;
+//	break; 
+//}
+//
+//	
+//		
+//	return 0;	
+//		
+//		
+//	}
+
+
+//{
+//	char x;
+//	cout<<"enter a number"<<endl;
+//	cin>>x;
+//		switch(x) {	
+//		case 0 :
+//			cout<<"the number is equal to 0"<<endl;
+//			break;
+//	     default: 
+//    if (x>0) {
+//    	cout<<"the number is positive"<<endl;
+//	}
+//		else {
+//			cout<<"the number is equal to 0"<<endl;
+////		} }
+//		return 0;	
+//	}
+//	
+
+//{ 
+//  float x;
+//  cout<<"enter the age of person"<<endl;
+//  cin>>x;
+//  if (x<20) {
+//  if (x>=13)	{
+//  	cout<<"the person is a teenager"<<endl;
+// 	  }
+// 	  else { 
+// 	  cout<<"the person is a child"<<endl;
+//	   }
+//  }
+//	else
+//	{
+//		 cout<<"the person is an adult"<<endl;
+//	}
+//	return 0;
+//}
+
+//{
+//	float x,y,z;
+//	cout<<"enter 1st number"<<endl;
+//	cin>>x;
+//		cout<<"enter 2nd number"<<endl;
+//	cin>>y;
+//		cout<<"enter 3rd number"<<endl;
+//	cin>>z;
+//	if (x!=y&&x!=z)  {
+//		if (x>y&&x>z) {
+//				cout<<"1st numberis the greatest of all"<<endl;
+//				
+//		}
+//			if (y>x&&y>z) {
+//				cout<<"2nd numberis the greatest of all"<<endl;
+//			}
+//			
+//				if (z>y&&z>x) {
+//				cout<<"3rd numberis the greatest of all"<<endl;
+//	}
+//
+//	 
+//	}
+//	else {
+//				cout<<"all the 3 numbers are equal"<<endl;
+//	}
+//}
+
+//{
+//	char x;
+//	cout<<"enter a character "<<endl;
+//	cin>>x;
+//	if ((x>='a'&&x<='z') || (x>='A'&&x<='Z')) {
+//		if (x=='a'|| x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U') {
+//			cout<<"given character is a vowel "<<endl;
+//		}
+//		else {
+//			cout<<"given character is a constant "<<endl;
+//		}
+//		
+//	}
+//	else {
+//		cout<<"given character is not an alphabet "<<endl;
+//	}
+//}
+//	
+	
